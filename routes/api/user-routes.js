76d494d67ecf6238401e48b45042239fd38615bb
will sherman <willsherman771@gmail.com>
2023-10-08 20:23:47 -0400

    more user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,13 @@
 
 const router = require('express').Router();
-const {getUsers, createUser} = require('../../controllers/user-controller');
+const {getUsers, createUser, deleteOneUser, updateUser} = require('../../controllers/user-controller');
 //localhostPORT/api/users
 router.route('/').get(getUsers).post(createUser);
+router.route('/').post(createUser);
+router.route("/:userId").delete(deleteOneUser);
+router.route("/;userId").put(updateUser);
+
+//router.route("/:userId/friends").post(createOneFriend);
+//router.route("/:userId/friends/:friendId").delete(deleteOneFriend);
 
 module.exports = router;
